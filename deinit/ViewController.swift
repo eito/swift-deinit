3ca8ad35d940e52f8f00f7d440a1536871f30ee7
Eric Ito <eric.ito@gmail.com>
2016-09-05 21:25:53 -0700

    Initial Commit

---
+++ deinit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  deinit
+//
+//  Created by Eric Ito on 9/5/16.
+//  Copyright © 2016 Eric Ito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
